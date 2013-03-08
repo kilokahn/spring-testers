f03bd4f36f3f64697b9aaaab7fc34f282597c323
kilokahn <mukarram.baig@gmail.com>
2013-03-08 11:13:24 +0530

    Removing headers

--- spring-cxf-jaxrs-headless-tester/src/main/java/com/kilo/CXFHeadlessDriver.java
+++ spring-cxf-jaxrs-headless-tester/src/main/java/com/kilo/CXFHeadlessDriver.java
@@ -1,31 +1,9 @@
-/*
- * CXFHeadlessDriver.java
- * 
- * $HeadURL: $
- * $Id: eclipse-codetemplates.xml 413456 2010-08-10 07:03:38Z baigm $
- */
-
-/*
- * Copyright (c) 2012 D. E. Shaw & Co., L.P. All rights reserved.
- *  
- * This software is the confidential and proprietary information
- * of D. E. Shaw & Co., L.P. ("Confidential Information").  You
- * shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement
- * you entered into with D. E. Shaw & Co., L.P.
- */
 
 package com.kilo;
 
 import org.apache.log4j.Logger;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-//@formatter:off
-/**
- * @author baigm
- * @version $Id: eclipse-codetemplates.xml 413456 2010-08-10 07:03:38Z baigm $
- */
-//@formatter:on
 public class CXFHeadlessDriver {
 
     private static Logger LOG = Logger.getLogger(CXFHeadlessDriver.class);
