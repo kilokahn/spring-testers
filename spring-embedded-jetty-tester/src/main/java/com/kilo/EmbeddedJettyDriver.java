ad31552b28eff060ed3fb3798d30999cab4bb290
kilokahn <mukarram.baig@gmail.com>
2013-06-06 12:05:24 +0530

    Embedded jetty server with CXF

--- spring-embedded-jetty-tester/src/main/java/com/kilo/EmbeddedJettyDriver.java
+++ spring-embedded-jetty-tester/src/main/java/com/kilo/EmbeddedJettyDriver.java
@@ -1,18 +1,38 @@
 
 package com.kilo;
 
-import org.apache.log4j.Logger;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StopWatch;
 
 public class EmbeddedJettyDriver {
 
-    private static Logger LOG = Logger.getLogger(EmbeddedJettyDriver.class);
+    private static Logger LOG = LoggerFactory
+            .getLogger(EmbeddedJettyDriver.class);
 
-    public static void main(String[] args) {
-        new ClassPathXmlApplicationContext(
-                "classpath:com/kilo/embedded/jetty/logging.xml",
-                "classpath:com/kilo/embedded/jetty/applicationContext.xml");
-        LOG.info("Inited");
+    public static void main(String[] args) throws Exception {
+
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Port number is mandatory");
+        }
+        Integer portNumber = Integer.parseInt(args[0]);
+        StopWatch sw = new StopWatch();
+        sw.start();
+        LOG.info("Starting driver context load");
+        Server server = new Server(portNumber);
+        WebAppContext root = new WebAppContext();
+        root.setContextPath("/");
+        root.setWar("src/main/webapp");
+
+        server.setHandler(root);
+
+        sw.stop();
+        LOG.info("Done driver context load in " + sw.getLastTaskTimeMillis());
+
+        server.start();
+        server.join();
+
     }
-
 }
--- spring-embedded-jetty-tester/src/main/java/com/kilo/SpecialServiceImpl.java
+++ spring-embedded-jetty-tester/src/main/java/com/kilo/SpecialServiceImpl.java
@@ -3,14 +3,8 @@ package com.kilo;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 
-import org.apache.cxf.rs.security.cors.CrossOriginResourceSharing;
-
-@Produces("application/json")
-@CrossOriginResourceSharing(allowAllOrigins = true, allowCredentials = true, maxAge = 6000, allowHeaders = {
-        "Content-Type", "X-Requested-With" }, exposeHeaders = { "Content-Type",
-        "X-Requested-With" })
+@Path("/specialService/")
 public class SpecialServiceImpl implements SpecialService {
 
     @GET
