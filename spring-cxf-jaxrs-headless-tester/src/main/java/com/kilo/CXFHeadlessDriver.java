f6a473b4c8c5847e44a88ed92de8b76dc8485410
kilokahn <mukarram.baig@gmail.com>
2014-05-20 20:22:46 +0530

    Sleep for an hour so that one can play with the instance brought up

--- spring-cxf-jaxrs-headless-tester/src/main/java/com/kilo/CXFHeadlessDriver.java
+++ spring-cxf-jaxrs-headless-tester/src/main/java/com/kilo/CXFHeadlessDriver.java
@@ -9,11 +9,13 @@ public class CXFHeadlessDriver {
 
     private static Logger LOG = Logger.getLogger(CXFHeadlessDriver.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         try (ConfigurableApplicationContext applicationContext = new GenericXmlApplicationContext(
                 "classpath:com/kilo/cxf/logging.xml",
                 "classpath:com/kilo/cxf/applicationContext.xml")) {
             LOG.info("Inited");
+            // Sleep for an hour while we play around
+            Thread.sleep(60* 60 * 1_000);
         }
     }
 }
