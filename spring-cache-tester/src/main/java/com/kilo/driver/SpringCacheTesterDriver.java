adae1e89647aa17a6e835f7933603ff61441faa8
kilokahn <mukarram.baig@gmail.com>
2014-05-20 18:36:13 +0530

    Spring cleanup
    1. Use the recommended GenericXmlApplicationContext instead of
    ClassPathXmlApplicationContext
    2. Use ConfigurableApplicationContext to close the application context
    using the try-with-resources directive in java 7.

--- spring-cache-tester/src/main/java/com/kilo/driver/SpringCacheTesterDriver.java
+++ spring-cache-tester/src/main/java/com/kilo/driver/SpringCacheTesterDriver.java
@@ -2,8 +2,8 @@
 package com.kilo.driver;
 
 import org.apache.log4j.Logger;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.util.StopWatch;
 
 import com.kilo.external.ExternalFibonacciServiceImpl;
@@ -14,30 +14,30 @@ public class SpringCacheTesterDriver {
 
     public static void main(String[] args) {
 
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+        try (ConfigurableApplicationContext applicationContext = new GenericXmlApplicationContext(
                 "classpath:com/kilo/spring/applicationContext.xml",
-                "classpath:com/kilo/spring/logging.xml");
-        FibonacciService simpletonFibonacciService = (FibonacciService) applicationContext
-                .getBean("simpletonFibonacciService");
-        FibonacciService concurrentMapFibonacciService = (FibonacciService) applicationContext
-                .getBean("concurrentMapFibonacciService");
-        FibonacciService ehcacheFibonacciService = (FibonacciService) applicationContext
-                .getBean("ehcacheFibonacciService");
-        FibonacciService externalFibonacciService = (FibonacciService) applicationContext
-                .getBean("externalFibonacciService");
+                "classpath:com/kilo/spring/logging.xml")) {
+            FibonacciService simpletonFibonacciService = (FibonacciService) applicationContext
+                    .getBean("simpletonFibonacciService");
+            FibonacciService concurrentMapFibonacciService = (FibonacciService) applicationContext
+                    .getBean("concurrentMapFibonacciService");
+            FibonacciService ehcacheFibonacciService = (FibonacciService) applicationContext
+                    .getBean("ehcacheFibonacciService");
+            FibonacciService externalFibonacciService = (FibonacciService) applicationContext
+                    .getBean("externalFibonacciService");
 
-        doWorkWithFiboService(simpletonFibonacciService);
-        doWorkWithFiboService(concurrentMapFibonacciService);
-        doWorkWithFiboService(ehcacheFibonacciService);
-        doWorkWithFiboService(externalFibonacciService);
+            doWorkWithFiboService(simpletonFibonacciService);
+            doWorkWithFiboService(concurrentMapFibonacciService);
+            doWorkWithFiboService(ehcacheFibonacciService);
+            doWorkWithFiboService(externalFibonacciService);
 
-        long fibonacci = ((ExternalFibonacciServiceImpl) externalFibonacciService)
-                .getFibonacci(45);
-        LOG.info("Fibonacci number is " + fibonacci);
-        long nonFibonacci = ((ExternalFibonacciServiceImpl) externalFibonacciService)
-                .getNonFibonacci(45);
-        LOG.info("Non-fibonacci number is " + nonFibonacci);
-
+            long fibonacci = ((ExternalFibonacciServiceImpl) externalFibonacciService)
+                    .getFibonacci(45);
+            LOG.info("Fibonacci number is " + fibonacci);
+            long nonFibonacci = ((ExternalFibonacciServiceImpl) externalFibonacciService)
+                    .getNonFibonacci(45);
+            LOG.info("Non-fibonacci number is " + nonFibonacci);
+        }
         System.exit(1);
     }
 
