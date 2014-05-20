9e0350404dc52558090c042fe2daaa738c4f11d5
kilokahn <mukarram.baig@gmail.com>
2014-05-20 19:35:18 +0530

    Spring cleanup
    1. Use the recommended GenericXmlApplicationContext instead of
    ClassPathXmlApplicationContext
    2. Use ConfigurableApplicationContext to close the application context
    using the try-with-resources directive in java 7.

--- spring-cxf-jaxrs-headless-tester/src/main/java/com/kilo/CXFHeadlessDriver.java
+++ spring-cxf-jaxrs-headless-tester/src/main/java/com/kilo/CXFHeadlessDriver.java
@@ -2,17 +2,18 @@
 package com.kilo;
 
 import org.apache.log4j.Logger;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class CXFHeadlessDriver {
 
     private static Logger LOG = Logger.getLogger(CXFHeadlessDriver.class);
 
     public static void main(String[] args) {
-        new ClassPathXmlApplicationContext(
+        try (ConfigurableApplicationContext applicationContext = new GenericXmlApplicationContext(
                 "classpath:com/kilo/cxf/logging.xml",
-                "classpath:com/kilo/cxf/applicationContext.xml");
-        LOG.info("Inited");
+                "classpath:com/kilo/cxf/applicationContext.xml")) {
+            LOG.info("Inited");
+        }
     }
-
 }
