1b949bb41c6cddb942886709cf17f51f11d64db8
kilokahn <mukarram.baig@gmail.com>
2016-05-09 16:00:30 -0400

    Mark the test case as abstract.

--- spring-jms-batched-listener-tester/src/test/java/com/kilo/BaseTest.java
+++ spring-jms-batched-listener-tester/src/test/java/com/kilo/BaseTest.java
@@ -9,6 +9,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(locations = {
         "/com/kilo/spring/test-applicationContext.xml",
         "/com/kilo/spring/logging.xml" })
-public class BaseTest {
+public abstract class BaseTest {
 
 }
